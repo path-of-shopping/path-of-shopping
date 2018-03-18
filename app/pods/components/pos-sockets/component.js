19e1faa2b97b422016c7d449859507a75473992f
Pascal Boutin <pboutin@mirego.com>
2018-03-18 15:00:42 -0400

    Fix sockets for 4 sockets items

--- app/pods/components/pos-sockets/component.js
+++ app/pods/components/pos-sockets/component.js
@@ -1,5 +1,6 @@
 import Component from '@ember/component';
 import {computed} from '@ember/object';
+import {lte} from '@ember/object/computed';
 
 const COLORS_MAP = {
   R: 'red',
@@ -9,15 +10,20 @@ const COLORS_MAP = {
 
 export default Component.extend({
   tagName: 'ul',
+  classNameBindings: ['isFourSockets:sockets--four'],
 
   sockets: [],
 
   displaySockets: computed('sockets', function() {
     const sockets = this.get('sockets');
 
+    if (!sockets) return [];
+
     return sockets.map(({group, color}, index) => ({
       color: COLORS_MAP[color],
       isLinked: (index + 1) < sockets.length && sockets[index + 1].group === group
     }));
-  })
+  }),
+
+  isFourSockets: lte('displaySockets.length', 4)
 });
