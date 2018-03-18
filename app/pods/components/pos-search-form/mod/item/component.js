9e6c8056b104ed3e53ffef9779308a4e731bfa46
Pascal Boutin <pboutin@mirego.com>
2018-03-17 21:34:25 -0400

    Hide weight for filters for other types

--- app/pods/components/pos-search-form/mod/item/component.js
+++ app/pods/components/pos-search-form/mod/item/component.js
@@ -1,14 +1,17 @@
 import Component from '@ember/component';
 import {inject as service} from '@ember/service';
-import {readOnly} from '@ember/object/computed';
+import {readOnly, equal} from '@ember/object/computed';
+import ModFilterTypes from 'pos/constants/mod-filter-types';
 
 export default Component.extend({
   staticDataFetcher: service('fetchers/static-data-fetcher'),
 
   item: null,
+  filterType: null,
   onModUpdate: () => {},
 
   mods: readOnly('staticDataFetcher.mods'),
+  isWeightRequired: equal('filterType', ModFilterTypes.WEIGHT),
 
   modSelect(mod) {
     this.set('item.mod', mod ? mod.get('id') : null);
