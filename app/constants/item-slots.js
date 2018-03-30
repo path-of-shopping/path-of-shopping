3cd8be1b960311c9b407129d5ac65b929b741929
Pascal Boutin <pboutin@mirego.com>
2018-03-30 12:58:42 -0400

    Introduce the item slots file

---
+++ app/constants/item-slots.js
@@ -0,0 +1,78 @@
+export default [
+  {
+    id: 'weapon',
+    label: 'Weapon'
+  },
+  {
+    id: 'shield',
+    label: 'Shield'
+  },
+  {
+    id: 'chest-armour',
+    label: 'Chest armour'
+  },
+  {
+    id: 'boots',
+    label: 'Boots'
+  },
+  {
+    id: 'gloves',
+    label: 'Gloves'
+  },
+  {
+    id: 'helmet',
+    label: 'Helmet'
+  },
+  {
+    id: 'quiver',
+    label: 'Quiver'
+  },
+  {
+    id: 'belt',
+    label: 'Belt'
+  },
+  {
+    id: 'amulet',
+    label: 'Amulet'
+  },
+  {
+    id: 'ring',
+    label: 'Ring'
+  },
+  {
+    id: 'jewel',
+    label: 'Jewel'
+  },
+  {
+    id: 'gem',
+    label: 'Gem'
+  },
+  {
+    id: 'flask',
+    label: 'Flask'
+  },
+  {
+    id: 'map',
+    label: 'Map'
+  },
+  {
+    id: 'leaguestone',
+    label: 'Leaguestone'
+  },
+  {
+    id: 'prophecy',
+    label: 'Prophecy'
+  },
+  {
+    id: 'card',
+    label: 'Card'
+  },
+  {
+    id: 'currency',
+    label: 'Currency'
+  },
+  {
+    id: 'beast',
+    label: 'Beast'
+  }
+];
